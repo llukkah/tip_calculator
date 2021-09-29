3bd7cec5578e68a3a0c2d023f2ec345ec07a8a29
llukkah <llukkah.rey@gmail.com>
2021-09-29 19:19:30 -0400

    added code for tip_calculator.py

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # The Tip Calculator
 
-### MVP 
+### MVP
+
 #### Make a python script (tip_calculator.py) that takes a user's input at the command line for:
 
 - Cost of the food
+- Assume there is a 10% sales tax and add to the total bill
 - Number of people splitting the bill
 - Percentage of the tip
-- The script should output: The total bill (including tip)
+- The script should output: The total bill (including tip) and how much each person should pay
+
 ---
+
 ### How to run the Tip Calculator
+
 In your Terminal:
+
 1. Clone this Repo with `git clone GITHUB URL`
 2. `cd` into the Repo folder
 3. Run `python3 tip_calculator.py`
---
+++ tip_calculator.py
@@ -0,0 +1,32 @@
+#total cost of food
+cost_of_food = int(input("Enter the total cost of food: "))
+
+#adding 10% sales tax
+total_food = (cost_of_food*.10) + cost_of_food
+
+#letting user know the total cost of food after adding 10% sales tax
+print(f'Okay, the total cost of food including 10% sales tax is $ {total_food}')
+
+#asking number of peopel splitting bill
+num_people = int(input("Now enter how many people splitting the bill: "))
+
+#asking the percentage of tip to add to bill
+percentage_tip = int(input("How much percentage do you want to add?: "))
+
+#converting the number to a decimal
+percentage_decimal = percentage_tip/100
+
+#making sure the decimal is created
+# print(f'percentage decimal: {percentage_decimal}')
+
+#doing the math to add the total cose of food and the amount of % to tip
+including_tip = total_food + (total_food*percentage_decimal)
+
+#making sure of the number of the food cost and including tip
+print(f'including tip : {including_tip}')
+
+#finding the amount each person should pay
+each_person_pay = including_tip / num_people
+
+#desired output
+print(f'The total bill including tip is $ {including_tip}.  Each person ({num_people}) should pay ${each_person_pay}')
