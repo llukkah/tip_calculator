09d2bfbf6771e3e3bba1a9ff1a5e463884e03f8f
llukkah <llukkah.rey@gmail.com>
2021-10-06 19:07:06 -0400

    added readME and edited logic

--- README.md
+++ README.md
@@ -19,3 +19,14 @@ In your Terminal:
 1. Clone this Repo with `git clone GITHUB URL`
 2. `cd` into the Repo folder
 3. Run `python3 tip_calculator.py`
+
+---
+
+### Cases the Tip Calculator should handle:
+
+| Case Name                   | Food Costs | Number People Paying | Tip Amount | Expected Output | Total Bill     | Each Person Pays |
+| --------------------------- | ---------- | -------------------- | ---------- | --------------- | -------------- | ---------------- |
+| A Meal for 1                | $ 15       | 1                    | 20%        |                 | $19.50         | $19.50           |
+| A feast to remember         | $25000000  | 3                    | 31%        |                 | $35,250,000.00 | $11,750,000.00   |
+| No tip                      | $78.99     | 6                    | No Tip     |                 | $86.89         | $14.48           |
+| Sharing the bill among many | $5000      | 876                  | 12%        |                 | $6,100.00      | $6.96            |
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,11 @@
-
-
 #total cost of food
-cost_of_food = int(input("Enter the total cost of food: "))
+cost_of_food = float(input("Enter the total cost of food: "))
 
 #adding 10% sales tax
-total_food = (cost_of_food*.10) + cost_of_food
+ten_tax = float((cost_of_food*.10))
 
 # letting user know the total cost of food after adding 10% sales tax
-print(f'Okay, the total cost of food including 10% sales tax is $ {total_food}')
+print(f'Okay, 10% sales tax of {cost_of_food} is $ {ten_tax}')
 
 #asking number of peopel splitting bill
 num_people = int(input("Now enter how many people splitting the bill: "))
@@ -16,19 +14,25 @@ num_people = int(input("Now enter how many people splitting the bill: "))
 percentage_tip = float(input("How much percentage do you want to add?: "))
 
 #converting the number to a decimal
-percentage_decimal = percentage_tip/100
+percentage_decimal = float(percentage_tip/100)
 
 #making sure the decimal is created
 # print(f'percentage decimal: {percentage_decimal}')
 
-#doing the math to add the total cose of food and the amount of % to tip
-including_tip = total_food + (total_food*percentage_decimal)
+#calculating tip of food cost
+tip_of_food = float(cost_of_food*percentage_decimal)
+
+#making sure tip of food
+# print(f'tip of food: {tip_of_food}')
+
+#doing the math to add the total cost of food, tax, and the amount of % to tip
+including_tip = float(ten_tax + tip_of_food + cost_of_food)
 
 #making sure of the number of the food cost and including tip
 # print(f'including tip : {including_tip}')
 
 #finding the amount each person should pay
-each_person_pay = including_tip / num_people
+each_person_pay = float(including_tip / num_people)
 
 #desired output
 print(f'The total bill including tip is $ {including_tip}.  Each person ({num_people}) should pay ${each_person_pay}')
